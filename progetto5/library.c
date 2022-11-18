5c79da7532c2b525f1f46e19228c147c32892400
musianisamuele <samuele.musiani.03@gmail.com>
2022-11-18 23:48:25 +0100

    Aggiunta degli array in cui sono contenute le tabelle di conversione per la C in.

--- progetto5/library.c
+++ progetto5/library.c
@@ -1,5 +1,73 @@
 #include "library.h"
 
+#ifndef PAIR_STRING
+#define PAIR_STRING
+
+struct pair_string {
+	char first[10];
+	char second[10];
+};
+
+typedef struct pair_string coppiaS;
+
+#endif
+
+void genera_conversioni (coppiaS *COMP, coppiaS *DEST, coppiaS *JUMP) {
+
+	coppiaS *p = (coppiaS*) malloc (18 * sizeof (coppiaS));
+
+	COMP = p;
+		
+	//COMP
+	strcpy ((*p).first, "0"); strcpy ((*p).second, "101010");		p++;
+	strcpy ((*p).first, "1"); strcpy ((*p).second, "111111"); 	p++;
+	strcpy ((*p).first, "-1"); strcpy ((*p).second, "111010");	p++;
+	strcpy ((*p).first, "D"); strcpy ((*p).second, "001100");		p++;
+	strcpy ((*p).first, "A"); strcpy ((*p).second, "110000");		p++;
+	strcpy ((*p).first, "!D"); strcpy ((*p).second, "001101");	p++;
+	strcpy ((*p).first, "!A"); strcpy ((*p).second, "110001");	p++;
+	strcpy ((*p).first, "-D"); strcpy ((*p).second, "001111");	p++;
+	strcpy ((*p).first, "-A"); strcpy ((*p).second, "110011");	p++;
+	strcpy ((*p).first, "D+1"); strcpy ((*p).second, "011111");	p++;
+	strcpy ((*p).first, "A+1"); strcpy ((*p).second, "110111");	p++;
+	strcpy ((*p).first, "D-1"); strcpy ((*p).second, "001110");	p++;
+	strcpy ((*p).first, "A-1"); strcpy ((*p).second, "110010");	p++;
+	strcpy ((*p).first, "D+A"); strcpy ((*p).second, "000010");	p++;
+	strcpy ((*p).first, "D-A"); strcpy ((*p).second, "010011");	p++;
+	strcpy ((*p).first, "A-D"); strcpy ((*p).second, "000111");	p++;
+	strcpy ((*p).first, "D&A"); strcpy ((*p).second, "000000");	p++;
+	strcpy ((*p).first, "D!A"); strcpy ((*p).second, "010101");
+
+	//DEST
+	p = (coppiaS*) malloc (8 * sizeof (coppiaS));
+	DEST = p;
+
+	strcpy ((*p).first, "null"); strcpy ((*p).second, "000");	p++;
+	strcpy ((*p).first, "M"); strcpy ((*p).second, "001");		p++;
+	strcpy ((*p).first, "D"); strcpy ((*p).second, "001");		p++;
+	strcpy ((*p).first, "MD"); strcpy ((*p).second, "010");		p++;
+	strcpy ((*p).first, "A"); strcpy ((*p).second, "100");		p++;
+	strcpy ((*p).first, "AM"); strcpy ((*p).second, "101");		p++;
+	strcpy ((*p).first, "AD"); strcpy ((*p).second, "110");		p++;
+	strcpy ((*p).first, "AMD"); strcpy ((*p).second, "111");	p++;
+
+	//JUMP
+	p = (coppiaS*) malloc (8 * sizeof (coppiaS));
+	JUMP = p;
+
+	strcpy ((*p).first, "null"); strcpy ((*p).second, "000");	p++;
+	strcpy ((*p).first, "JTG"); strcpy ((*p).second, "001");	p++;
+	strcpy ((*p).first, "JEQ"); strcpy ((*p).second, "001");	p++;
+	strcpy ((*p).first, "JGE"); strcpy ((*p).second, "010");	p++;
+	strcpy ((*p).first, "JLT"); strcpy ((*p).second, "100");	p++;
+	strcpy ((*p).first, "JNE"); strcpy ((*p).second, "101");	p++;
+	strcpy ((*p).first, "JLE"); strcpy ((*p).second, "110");	p++;
+	strcpy ((*p).first, "JMP"); strcpy ((*p).second, "111");	p++;
+
+}
+
+
+
 int traduci_file (FILE* fileI, FILE* fileO) {
 	int global_check = 1; //0 -> false, N\{0} -> True
 
@@ -8,7 +76,7 @@ int traduci_file (FILE* fileI, FILE* fileO) {
 	char I[MAX_RIGA];
 
 	while (!feof (fileI)) {		//Finché posso leggere -> feof(...) = True quando il file è finito
-		fgets (I, MAX_RIGA, fileI);
+		fgets (I, MAX_RIGA - 1, fileI);
 
 		//printf ("LEN: %d\n", strlen(tmp));
 		//printf ("tmp: %s\n", tmp);
@@ -18,17 +86,20 @@ int traduci_file (FILE* fileI, FILE* fileO) {
 
 			char O[17];
 			O[16] = '\0';
+			
+			strcpy (O, "2222222222222222");
 
 			if (instruction_type == 0) {
 
 				traduci_A_instruction (I, O);
 				
-				printf ("A isntruction rilevata\n");
-				printf ("\t %s = %s\n", I, O);
-
 			} else if (instruction_type == 1) {
 				//traduci_C_instruction (tmp);
+				
+				traduci_C_instruction (I, O);
+
 				printf ("C isntruction rilevata\n");
+				printf ("Tradotta: %s\n", O);
 			} else if (instruction_type == 2) {
 				printf ("Labels rilevata");
 			} else if (instruction_type == 3) {
@@ -80,6 +151,13 @@ void traduci_A_instruction (char I[], char O[]) {
 
 	dec_to_stringBin (n, O);
 
+}
+
+void traduci_C_instruction (char I[], char O[]) {
+	O[0] = 1; O[1] = 1; O[2] = 1; //I primi 3 bit di una C in. sono sempre a 1
+	O[3] = '\0';
+
+	//L'idea è di cercare dentro O tutte le possibili stringe e poi concatenare i risulati con strcat();
 }
 
 void dec_to_stringBin (int n, char O[]) {
--- progetto5/library.h
+++ progetto5/library.h
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef PAIR_STRING
+#define PAIR_STRING
+
+struct pair_string {
+	char first[10];
+	char second[10];
+};
+
+typedef struct pair_string coppiaS;
+
+#endif
+
 
 int traduci_file (FILE* fileI, FILE* fileO);
 
@@ -14,3 +26,7 @@ int detect_instruction_type (char I[]);
 
 void traduci_A_instruction (char I[], char O[]);
 void dec_to_stringBin (int n, char O[]);
+
+void traduci_C_instruction (char I[], char O[]);
+
+void genera_conversioni (coppiaS *COMP, coppiaS *DEST, coppiaS *JUMP);
