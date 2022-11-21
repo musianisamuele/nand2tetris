f969c4b92725ec9c90b4800c62d40109b7cfcf31
musianisamuele <samuele.musiani.03@gmail.com>
2022-11-21 17:52:30 +0100

    Ulteriore suddivisione di library in string.c

--- progetto5/library.c
+++ progetto5/library.c
@@ -24,13 +24,6 @@ plista get_labels (plista h, FILE* fileI) {
 	return (h);
 }
 
-void rimuovi_parentesi (char I[]) {
-	for (int i = 0; i < strlen (I) - 1; i++) {
-		I[i] = I[i + 1];
-	}
-
-	I[ strlen (I) - 4] = '\0';
-}
 
 int traduci_file (FILE* fileI, FILE* fileO) {
 	int global_check = 1; //0 -> false, N\{0} -> True
@@ -226,51 +219,4 @@ void traduci_C_instruction (char I[], char O[], coppiaS* CONV) {
 	if (tradotta == 0) {
 		strcat (O, "000");
 	}
-}
-
-void dec_to_stringBin (int n, char O[]) {
-	int tmp = (1 << 14); //2^14;
-
-	int indice = 1;
-	
-	while (indice < 16) {
-		//printf ("\nindice: %d\nn: %d\ntmp: %d\nO[]: %s\n", indice, n, tmp, O);
-		
-		if (n >= tmp) {
-			n = n - tmp;
-			O[indice] = '1';
-		} else {
-			O[indice] = '0';
-		}
-
-		tmp = (tmp >> 1);
-		indice = indice + 1;
-	}
-	
-}
-
-int is_in_string (char s[], char f[], int a, int b) {
-	int found = 0;
-
-	int i;
-	int j;
-	int lenS = strlen(s);
-	int lenF = strlen(f);
-
-	i = a;
-	while (i < lenS && found == 0 && i < b) {
-		if (s[i] == f[0] && lenS >= i + lenF ) {
-			found = 1;
-			j = 0;
-			while (j < lenF) {
-				if (s[i + j] != f[j])
-					found = 0;
-				
-				j = j + 1;
-			}
-		}
-		i = i + 1;
-	}
-
-	return (found);
 }
