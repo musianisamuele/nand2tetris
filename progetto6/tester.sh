1a20667ed4b34134e2a1e027894d72f5dc684e84
musianisamuele <samuele.musiani.03@gmail.com>
2022-12-04 15:09:20 +0100

    Aggiunto script sh per testare più velocemente le soluzioni

---
+++ progetto6/tester.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for file in test_files/*.vm
+do
+	./VMtranslator ${file}
+done
+
+for file in test_files/*.tst
+do
+	printf "\nComparazione di ${file}\n"
+	../../tools/CPUEmulator.sh ${file}
+done
